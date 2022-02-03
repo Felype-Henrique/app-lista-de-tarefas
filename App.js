fcb48da63d83d7cb0ff3c6e1c21b2021b28f4939
Felype Henrique <felypehenrique25@gmail.com>
2022-02-03 11:22:22 -0300

    section

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Tarefas de Hoje*/}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Tarefas de Hoje</Text>
+
+        <View styles={styles.items}>
+          {/*Tarefas para fazer*/}
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {},
 });
