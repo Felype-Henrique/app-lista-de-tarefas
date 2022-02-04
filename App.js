6c736df984f705ecd8a5bbd1d35a6ee71113d48b
Felype Henrique <felypehenrique25@gmail.com>
2022-02-04 12:16:45 -0300

    task state

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+
   return (
     <View style={styles.container}>
 
